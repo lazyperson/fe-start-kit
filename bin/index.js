e8c955d937407c3a7fdde5774b575cbe5e36a34b
lazyperson <564981089@qq.com>
2022-04-13 00:50:01 +0800

    :hammer: 重构

---
+++ bin/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var cmder = require('commander');
+var lib = require('../lib');
+
+var packageJson = require('../package.json');
+
+cmder.version(packageJson.version, '-v, --version')
+    .command('init <name>')
+    .action(name => {
+        lib.init(name);
+    });
+
+
+// 模板列表
+cmder.command('help')
+    .description('查看所有可用命令')
+    .action(() => {
+        console.log(`
+      fe-start-kit init <name>  初始化项目
+        `);
+    });
+
+
+cmder.parse(process.argv);
+
+
+if (!cmder.args.length) {
+    cmder.help();
+}
